e766bc9d50bb2b56247d95528d20688bbeeffadc
João Pedro <joao.cordeirocarvalho@gmail.com>
2022-03-02 23:07:27 -0300

    projeto obtendo-hora-sistema

---
+++ HoraDoSistema/src/HoraDoSistema.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class HoraDoSistema {
+	
+	public static void main(String[] args) {
+		
+		Date relogio = new Date();
+		
+		System.out.print("A hora do sistema é:");
+		System.out.println(relogio.toString());
+	}
+
+}
